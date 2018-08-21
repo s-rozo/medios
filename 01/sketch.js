6767714c517be6e68c39d2000cae459c22513d10
s-rozo <s.rozo@uniandes.edu.co>
2018-08-21 14:36:54 -0500

    Subir archivos ejercicio 01

---
+++ 01/sketch.js
@@ -0,0 +1,116 @@
+
+function setup() {
+  createCanvas(600, 600);
+	background (232,214, 167);
+	stroke(0);
+	
+	fill(189,87,98);
+	ellipse (200,340, 40,40);
+	
+	fill (240);
+	ellipse(215,530,150,150);
+	
+	fill(0);
+	ellipse (215,530,100,100);
+	
+	fill (240);
+	stroke (189,87,98);
+	strokeWeight (5);
+	ellipse (215,530,80,80);
+	
+	stroke(0);
+	strokeWeight(4);
+	fill(240);
+	triangle(290,640,630,650,470,30);
+	
+	strokeWeight(2);
+	line(230,250,600,400);
+	line(235,180,600,390);
+	
+	strokeWeight(1);
+	fill(129,127,130);
+	ellipse (332,180,60,60);
+	
+	noStroke();
+	fill(163,182,197,140);
+	ellipse(470,420,80,80);
+	
+	fill(230);
+	stroke(0);
+	ellipse(470,420,60,60);
+	point(470,420);
+	point(480,410);
+	point(460,410);
+	point(480,420);
+	point(470,405);
+	point(470,412);
+	point(460,420);
+	point(470,435);
+	point(470,428);
+	point(460,430);
+	point(480,430);
+	point(485,427);
+	
+	fill(205,186,57,220);
+	noStroke();
+	triangle (470,220,580,220,530,140);
+	
+	noFill();
+	stroke(0);
+	strokeWeight(4);
+	triangle(290,640,630,650,470,30);
+	
+	strokeWeight(2);
+	noFill();
+	arc(680,550,1300,1070,PI,PI+QUARTER_PI);
+	arc(595,570,1100,900,PI,PI+QUARTER_PI);
+	
+	strokeWeight(2);
+	fill(230);
+	ellipse(600,249,60,60);
+	
+	noStroke();
+	fill(118,135,181);
+	ellipse(600, 249, 40, 40);
+	
+	noFill();
+	strokeWeight(1);
+	stroke(0);
+	ellipse(600,249,55,55);
+	ellipse(600,249,50,50);
+	
+	strokeWeight(7);
+	stroke(189,87,98);
+	line(465,600,600,525);
+	
+	strokeWeight(3);
+	stroke(0);
+	line(470,600,600,530);
+	
+	
+	strokeWeight(10);
+	stroke(132,173,118);
+	triangle(383,620,614,620,462,523);
+	
+	stroke(0);
+	triangle(370,620,620,620,460,510);
+	
+	strokeWeight(3);
+	line(550,600,600,550);
+	line(365,600,540,500);
+	
+	strokeWeight(1);
+	line(350,600,535,493);
+	
+	arc(120,730,450,300,PI,0-HALF_PI);
+	
+	stroke(189,87,98);
+	strokeWeight(10);
+	ellipse(625,600,70,110);
+	
+	stroke(0);
+	strokeWeight(3);
+	ellipse(625,600,60,110);
+	
+}
+
