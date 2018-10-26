a1a7919f51bf9684271651ceffbd044f711ebc94
s-rozo <s.rozo@uniandes.edu.co>
2018-10-26 11:01:50 -0500

    Version Final(?)

--- 09/sketch.js
+++ 09/sketch.js
@@ -4,6 +4,11 @@ var gameState;
 var soundtrack;
 var score = [];
 var opTexto = 255;
+var opTexto2 = 0;
+var soundtrack;
+var pops;
+var played;
+var amp;
 
 //Variables para el jugador
 var pImg;
@@ -14,27 +19,45 @@ var opPlayer = 0;
 var pTamx;
 var pTamy;
 
-//Variables anillos puas
-var ring;
-var opRing = 0;
-
 //variables clavos
-var clavosP = [];
+var caidos = [];
 var clavos = [];
+var velCla;
+var imgCla;
+
+
+//Variables Pistola
+var balasP = [];
+var balas = [];
+var tamBx, tamBy;
+var velBal;
+var imgBal
+
+//Variable Roca rebotadora
+var bouncing;
+var r;
+var xB = 320;
+var yB = 180;
+var xspeed = 5;
+var yspeed = 2;
+var imgBouncer;
+
 
 //Cargar imagenes y varios efectos de sonido
-function preload()
-{
-	pImg = loadImage('assets/player.png');
+function preload() {
+  pImg = loadImage('assets/player.png');
+  imgCla = loadImage('assets/Clavo.png')
+  imgBal = loadImage('assets/Bullet.png')
+  imgBouncer = loadImage('assets/Bouncer.png')
+  soundtrack = loadSound('assets/soundtrack.mp3');
+  pops = loadSound('assets/POP.mp3');
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-  //Iniciar el juego en la introduccion
-  gameState = 0;
-  
+  angleMode(DEGREES);
+
   //Guardar la ubicacion para las estrellas
   for (var i = 0; i < 200; i++) {
     estrellas.push({
@@ -42,20 +65,11 @@ function setup() {
       y: random(0, height)
     });
   }
-  
-  //Crear las posiciones de los clavos
-  for(var s = 0; s < 20; s++)
-  {
-   clavosP.push({
-   	x:s* width/20,
-    y: 0,
-   });
-  }
-  //Posicion y tamano inicial del jugador
-  x = width/2;
-  y = height/2;
-  pTamx = (100*width)/2000;
-  pTamy = (75*height)/2000;
+
+  //Metodo que reinicia el juego
+  restart();
+
+
 }
 
 function draw() {
@@ -67,26 +81,150 @@ function draw() {
     ellipse(estrellas[e].x, estrellas[e].y, (8 * width) / 2000, (8 * width) / 2000);
     pop();
   }
+  //Titulo introductorio
+  fill(255, opTexto);
+  stroke(255, opTexto);
+  textSize((250 * width) / 2000);
+  textFont('Fascinate Inline');
+  textAlign(CENTER);
+  text('BUBBLE\nPOP', width / 2, height / 4);
+
+  //Boton de jugar
+  push();
+  fill(100, 0, 100, opTexto);
+  strokeWeight(8);
+  rectMode(CENTER);
+  rect(width / 2, height / 1.5, (500 * width) / 2000, (200 * height) / 2000, 50);
+  noFill();
+  stroke(255, 255, 255, opTexto - 35);
+  strokeWeight(5);
+  rect(width / 2, height / 1.5, (545 * width) / 2000, (245 * height) / 2000, 50);
+  stroke(255, 255, 255, opTexto - 84);
+  strokeWeight(3);
+  rect(width / 2, height / 1.5, (575 * width) / 2000, (275 * height) / 2000, 50);
+  pop();
+  push();
+  noStroke();
+  textAlign(CENTER);
+  textSize((100 * width) / 2000);
+  text('Play', width / 2, height / 1.45);
+  pop();
+
+  //autor del juego
+  push();
+  noStroke();
+  textFont('Helvetica');
+  textSize((30 * width) / 2000);
+  text('by: Santiago Rozo Urreta', width / 2, height / 1.05);
+  pop();
+
+  if (gameState == 1) {
+    //inicia al jugador con su burbuja
+    playerBub = new player(x, y);
+    playerBub.blow();
+
+    //Display de los clavos
+    for (var n = 0; n < clavos.length; n++) {
+      clavos[n].hang();
+    }
+    
+    //Cambios en la velocidad de movimiento de acuerda al tiempo
+     velCla+=0.01;
+      if(score[0]>100)
+      {
+       velBal+=0.01; 
+      }
+
+  //Establecimiento de los clavos y balas que deben caer
+    if (amp.getLevel() >= 0.5) {
+      for (var t = 0; t < 5; t++) {
+        caidos[t] = floor(random(0, clavos.length));
+      }
+    }
+    for (var t = 0; t < 5; t++) {
+      balasP[t] = floor(random(0, balas.length));
+    }
+		
+    //Display de las balas
+    if (score[0] >= 100) {
+      for (var l = 0; l < balas.length; l++) {
+        balas[l].load();
+      }
+    }
+
+    //Movimiento de las balas y los clavos dependiendo de la cancion
+    for (var c = 0; c < caidos.length; c++) {
+      clavos[caidos[c]].fall();
+      if(score[0] > 100)
+      {
+      balas[balasP[c]].shoot();
+      }
+    }
+
+    //Creaci[on de la pelota rebotadora despues de cierto tiempo
+    if (score[0] > 500) {
+      push();
+      fill(155, opPlayer);
+      noStroke();
+      image(imgBouncer, xB,yB, r*2,r*2);
+      xB += xspeed;
+      yB += yspeed;
+      if (xB > width - r || xB < r) {
+        xspeed = -xspeed;
+      }
+      if (yB > height - r || yB < r) {
+        yspeed = -yspeed;
+      }
+      pop();
+
+    }
+		
+    //Calculo y muestra del puntaje
+    score[0] = floor(millis() / 100 - score[1]);
+    push();
+    textSize((50 * width) / 2000);
+    fill(255);
+    text('Score: ' + score[0], width / 1.1, height / 15);
+
+    //Transicion de estados. Desaparecen los textos introductorios y comienza el juego
+    if (gameState == 1 && opTexto > 0) {
+      opTexto -= 5;
+      opPlayer += 5;
+    }
+
+
+  }
   
-	//Titulo introductorio
-    fill(255, opTexto);
+  //Metodo que controla si el jugador pierde
+  loss();
+
+  //Estado de perder donde aparece el aviso y el boton de reinicio
+  if (gameState == 2) {
+    opTexto2++;
+
+    fill(255, opTexto2);
     stroke(255, opTexto);
     textSize((250 * width) / 2000);
     textFont('Fascinate Inline');
     textAlign(CENTER);
-    text('BUBBLE\nPOP', width / 2, height / 4);
-
-	//Boton de jugar
+    text('YOU\nLOSE', width / 2, height / 4);
+    
     push();
-    fill(100, 0, 100,opTexto);
+    textSize(21)
+    text('Score: ' + score[2], width/2, height/1.8);
+    pop();
+
+    //Boton de ir al menu
+    push();
+    fill(100, 0, 100, opTexto2);
     strokeWeight(8);
     rectMode(CENTER);
     rect(width / 2, height / 1.5, (500 * width) / 2000, (200 * height) / 2000, 50);
     noFill();
-    stroke(255, 255, 255, opTexto-35);
+    stroke(255, 255, 255, opTexto2 - 35);
     strokeWeight(5);
     rect(width / 2, height / 1.5, (545 * width) / 2000, (245 * height) / 2000, 50);
-    stroke(255, 255, 255, opTexto-84);
+    stroke(255, 255, 255, opTexto2 - 84);
     strokeWeight(3);
     rect(width / 2, height / 1.5, (575 * width) / 2000, (275 * height) / 2000, 50);
     pop();
@@ -94,147 +232,186 @@ function draw() {
     noStroke();
     textAlign(CENTER);
     textSize((100 * width) / 2000);
-    text('Play', width / 2, height / 1.45);
+    text('Retry', width / 2, height / 1.45);
     pop();
-
-  //autor del juego
-    push();
-    noStroke();
-    textFont('Helvetica');
-    textSize((30 * width) / 2000);
-    text('by: Santiago Rozo Urreta', width / 2, height / 1.05);
-    pop();
-  
-  //inicia al jugador con su burbuja
-  playerBub = new player(x,y);
-  playerBub.blow();
-  
-   for (var i = 0; i < 6; i++) 
-   {
-  push();
-	translate(width/2, height/2);
-  translate(p5.Vector.fromAngle((i*200)-millis() / 1000, (500*width)/2000));
-  ring = new rings(0,0);
-  ring.spin();
-  pop();
   }
-  
-  for(var n = 0; n < nails.length; n++)
-      {
-        clavos[n] = new nails (clavosP[n].x, clavosP[n].y);
-        clavos[n].hang();
-      }
-  
-  //Transicion de estados
-  if(gameState == 1 && opTexto > 0)
-  {
-    opTexto-=5;
-    opPlayer+=5;
-    opRing += 5;
-  }
-  if(gameState == 1)
-  {
-    score[0] = floor(millis()/100-score[1]);
-    push();
-    textSize((50*width)/2000);
-		fill(255);
-    text('Score: ' + score[0], width/1.1, height/15);
-    
-  }
-  
-  console.log(gameState);
+  console.log(velCla);
 }
 
-function player(pX, pY) 
-{
+//Calse jugador para la creaciony movimiento del jugados
+function player(pX, pY) {
   this.x = pX;
   this.y = pY;
   this.perdio = false;
-  
-  
-  this.blow = function()
-  {
+
+
+  this.blow = function() {
     imageMode(CENTER);
-    tint(255,opPlayer);
-  	image(pImg,x,y,pTamx,pTamy);
+    tint(255, opPlayer);
+    image(pImg, x, y, pTamx, pTamy);
   }
 
 }
 
-function rings(rX, rY) 
-{
-	this.x = rX;
-  this.y = rY;
-  
-  this.spin = function()
-  {
-      for (var i = 0; i < 4; i++) {
-      push();
-      translate(this.x, this.y);
-      translate(p5.Vector.fromAngle((i * 300) - millis() / 2000, (200*width)/2000));
-      fill(255,opRing);
-      noStroke();
-      ellipse(0,0, (50*width)/2000, (50*height)/2000);
-      pop();
+//Clase clavos parala crecion y movimiento de los clavos
+function nails(sX, sY) {
+  this.x = sX;
+  this.y = sY;
+
+  this.hang = function() {
+    fill(255, 0, 0, opPlayer);
+    noStroke();
+    image(imgCla, this.x,this.y, (75*width)/2000,(125*height)/2000);
+  }
+
+  this.fall = function() {
+    if (this.y >= height) {
+      this.y = sY;
+    }
+    this.y += velCla;
+  }
+
+  this.giveX = function() {
+    return this.x;
+  }
+
+  this.giveY = function() {
+    return this.y;
+  }
+}
+
+
+//Clase creada para las balas en el juego
+function bullets(bX, bY) {
+  this.x = bX;
+  this.y = bY;
+  this.showing = false;
+
+  this.load = function() {
+    fill(0, 255, 0, opPlayer);
+    noStroke();
+    image(imgBal, this.x,this.y, tamBx,tamBy);
+    this.showing = true;
+  }
+
+  this.shoot = function() {
+    if (this.x <= 0) {
+      this.x = bX;
+    }
+    this.x -= velBal;
+
+  }
+
+  this.isShowing = function() {
+    return this.showing;
+  }
+
+}
+
+function loss() {
+  //Condicional para perder por la cercania de los clavos
+  for (var p = 0; p < clavos.length; p++) {
+    var dista = dist(x, y, clavos[p].x, clavos[p].y)
+    if (dista <= pTamx / 2 + (34 * height / 2000)) {
+      if(!pops.isPlaying() && !played)
+      {
+      pops.play();
+        played = true;
+      }
+      gameState = 2;
+      score[2] = score[0];
     }
   }
-
+//Condicional para perder por la cercania de las balas
+  for (var h = 0; h < balas.length; h++) {
+    if (balas[h].isShowing) {
+      if (dist(x, y, balas[h].x, balas[h].y) <= tamBx) {
+          if(!pops.isPlaying()&&!played)
+      {
+      pops.play();
+        played = true;
+      }
+        gameState = 2;
+        score[2] = score[0];
+      }
+    }
+  }
+//Condicional para perder por la pelota que rebota
+  var reb = dist(x, y, xB, yB);
+  if (reb <= pTamx / 2 + r) { 
+    if(!pops.isPlaying() && !played)
+      {
+      pops.play();
+        played = true;
+      }
+    gameState = 2;
+    score[2] = score[0];
+  }
 }
 
-function follower() {
-
-
-}
-
-function nails(sX, sY) {
-this.x = sX;
-this.y = sY;
-  
-this.hang = function()
-{
-  fill(255,0,0,opPlayer);
-	rect(this.x, this.y, (50*width)/2000, (100*height)/2000);
-}
-
-this.fall = function()
-{
- if(this.y >= height)
- {
-  this.y = sY;
- }
-this.y--;
-}
-}
-
-function bounce() {
-
-
-}
-
-function lines() {
-
-
-}
-
-function mousePressed() 
-{
-  if(gameState == 0)
-  {
-    if (dist(mouseX,mouseY, width/2, height/1.5) <= (200*width)/2000)
-    { 
+//Cambios de estado depenediendo del estado y boton oprimidos
+function mousePressed() {
+  if (gameState == 0) {
+    if (dist(mouseX, mouseY, width / 2, height / 1.5) <= (200 * width) / 2000) {
       gameState = 1;
-      score[1] = millis()/100;
+      score[1] = millis() / 100;
       score[0] = 0;
     }
+  } else if (gameState == 2) {
+    if (dist(mouseX, mouseY, width / 2, height / 1.5) <= (200 * width) / 2000) {
+      restart();
+    }
   }
- 
+
 }
 
-function mouseDragged()
-{
- 	if(dist(mouseX,mouseY,x,y) < pTamx/2)
-  {
+//Movimiento del jugador por medio del dedo
+function mouseDragged() {
+  if (dist(mouseX, mouseY, x, y) < pTamx / 2) {
     x = mouseX;
     y = mouseY;
   }
+}
+
+function restart() {
+  //Reinicio del soundtrack e iniciacion
+  if (soundtrack.isPlaying()) {
+    soundtrack.jump(0)
+  } else {
+    soundtrack.play();
+  }
+  amp = new p5.Amplitude();
+  played = false;
+
+  //Iniciar el juego en la introduccion
+  gameState = 0;
+
+  //opacidad de los textos para que vayan desapareciendo y apareciedno
+  opTexto = 255;
+  opTexto2 = 0;
+
+  //Crear las posiciones de los clavos y establecer velocidad
+  for (var s = 0; s < 9; s++) {
+    clavos[s] = new nails((s + 1) * width / 10, 0);
+  }
+  velCla = 5;
+
+  //Posiciones balas y velocidades
+  tamBx = (75 * width) / 2000;
+  tamBy = (50 * height) / 2000;
+  for (var b = 0; b < 9; b++) {
+    balas[b] = new bullets(width, (b + 1) * height / 10);
+  }
+
+  r = (100 * width) / 2000;
+  velBal =5;
+
+  //Posicion y tamano inicial del jugador
+  x = width / 2;
+  y = height / 2;
+  pTamx = (100 * width) / 2000;
+  pTamy = (100 * width) / 2000;
+
+  xR = width / 2;
+  xY = height / 2;
 }
